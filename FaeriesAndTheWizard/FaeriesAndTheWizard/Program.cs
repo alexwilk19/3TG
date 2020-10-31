56e941cc96c4039ebf15adc235e3e1c943f9bab4
Alex Wilkinson <alex.wilk@outlook.com>
2020-10-31 16:28:10 +0000

    TrainingRoomLayout commit 10

--- FaeriesAndTheWizard/FaeriesAndTheWizard/Program.cs
+++ FaeriesAndTheWizard/FaeriesAndTheWizard/Program.cs
@@ -11,63 +11,7 @@ namespace FaeriesAndTheWizard
     {
         static void Main(string[] args)
         {
-            Random rand = new Random();
-            for (int row = 0; row < 20; row++)
-            {
-                for (int col = 0; col < 41; col++)
-                {
-                    if (row == 0 || row == 19)//top or bottom rows
-                    {
-
-                        if (col == 0 || col == 40)
-                        {
-                            Console.Write("|");
-                        }
-                        else
-                        {
-                            if (col == 18 )
-                            {
-                                Console.Write("<");
-                            }
-                            else if (col == 22)
-                            {
-                                Console.Write(">");
-                            }
-                            else
-                            {
-                                Console.Write("-");
-                            }
-                         
-                        }
-                    }
-      
-                    else
-                    {
-                      
-                         if (col == 0 || col == 40)
-                        {
-                            Console.Write("|");
-                        }
-                        else
-                        {
-                            if (row == 18 && col == 20)
-                            {
-                               
-                                    Console.Write("@");
-                                
-
-                            }
-                            else
-                            {
-                                 Console.Write(" ");
-                            }
-                       
-                        }
-                    }
-                }
-                Console.Write("\n");
-
-            }
+          
             Processor.ProcessText(TextStorage.IntroText, 20);
 
             Wizard player = new Wizard(Console.ReadLine());
@@ -76,7 +20,7 @@ namespace FaeriesAndTheWizard
             Thread.Sleep(600);
             Processor.ProcessText(TextStorage.FirstChoicesT1, 20);
             Processor.PresentOptions(TextStorage.FirstChoices);
-            Rooms AllRooms = new Rooms(10);
+       
 
             switch (Console.ReadLine())
             {
@@ -85,7 +29,7 @@ namespace FaeriesAndTheWizard
                     Processor.ProcessText(TextStorage.FirstChoiceO12, 20);
                     if (Console.ReadLine() == "r")
                     {
-                        Processor.PresentOptions(TextStorage.TrainingRoomDraw);
+                        Room TrainingRoom = new Room(1);
                     }
                     break;
                 default:
