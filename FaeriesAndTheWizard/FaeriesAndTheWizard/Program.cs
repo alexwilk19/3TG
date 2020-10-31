da0f75cc77b69edb3cf1e131e47f2f2d1397bfba
Alex Wilkinson <alex.wilk@outlook.com>
2020-10-31 19:06:02 +0000

    Commit 20

--- FaeriesAndTheWizard/FaeriesAndTheWizard/Program.cs
+++ FaeriesAndTheWizard/FaeriesAndTheWizard/Program.cs
@@ -9,6 +9,29 @@ namespace FaeriesAndTheWizard
 {
     class Program
     {
+        private void Combat(ref Wizard player, int eHP, int eDMG)
+        {
+            Faerie enemy = new Faerie(eHP, eDMG);
+            Console.WriteLine("\n\n\n\n");
+            Processor.ProcessText("An evil fae! It wants to kill you for using its kindred as reagents. How foolish. Show them who has the superior magic!",10);         
+            Console.WriteLine("\n");
+            Processor.ProcessText("Choose an ability to attack them:\n" +
+                "1. Staff Strike --- You deal between 10-20 damage, with no miss chance.\n" +
+                "2. Fireball --- You deal between 1-100 damage, with a chance to instead damage yourself!\n" +
+                "3. Ice Strike --- You deal 30 damage, with a 20% chance to deal double damage, or miss.\n", 10);
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+                default:
+                    break;
+            }
+        }
         static void Main(string[] args)
         {
 
@@ -23,7 +46,7 @@ namespace FaeriesAndTheWizard
             Processor.ProcessText(TextStorage.FirstChoiceO12, 20);
         NewRoom:
             Random randy = new Random();
-            Room TrainingRoom = new Room(randy.Next(19000000));
+            Room TrainingRoom = new Room(randy.Next(1,19000000));
             string newRoomText = "Ah, a lower floor of the tower.\nAt a quick glance, this room contains ";
             int enemyNumberPerRoom = 0;
             int itemNumberPerRoom = 0;
@@ -64,30 +87,74 @@ namespace FaeriesAndTheWizard
             Processor.ProcessText($"Score: {player._Score}", 10);
             Processor.ProcessText($"Room Number: {player._RoomsCleared + 1}/30", 10);
             Processor.ProcessText(newRoomText, 10);
-
-            if (TrainingRoom.Contents.ContainsKey(Convert.ToInt32(Console.ReadLine())))
+            int choice = Convert.ToInt32(Console.ReadLine());
+            if (TrainingRoom.Contents.ContainsKey(choice))
             {
-                Random rand = new Random();
-                int result = rand.Next(2);
-                switch (result)
+                if (TrainingRoom.Contents[choice].Contains("I"))
                 {
-                    case 0:
-                        player._Score += 10;
-                        Processor.ProcessText("You have found a potion! Plus 10 points!", 10);
-                        break;
-                    case 1:
-                        player._Score += 20;
-                        Processor.ProcessText("You have found a trinket! Plus 20 points!", 20);
-                        break;
-                    case 2:
-                        player._Score += 30;
-                        Processor.ProcessText("You have found an artifact! Plus 30 point!", 30);
-                        break;
-                    default:
-                        break;
+                    Random rand = new Random();
+                    int result = rand.Next(2);
+          
+                    switch (result)
+                    {
+                        case 0:
+                            player._Score += 10;
+                            Processor.ProcessText("You have found a potion! Plus 10 points!", 10);
+                            break;
+                        case 1:
+                            player._Score += 20;
+                            Processor.ProcessText("You have found a trinket! Plus 20 points!", 20);
+                            break;
+                        case 2:
+                            player._Score += 30;
+                            Processor.ProcessText("You have found an artifact! Plus 30 point!", 30);
+                            break;
+                        default:
+                            break;
+                    }
                 }
+                else if(TrainingRoom.Contents[choice].Contains("F")) //combat stuff happens in here
+                {
+                    Random crand = new Random();
+                    int faeHP = 0;
+                    int faeDMG = 0;
+                    switch (crand.Next(2))
+                    {
+                        case 0:
+                            faeHP = 30;
+                            break;
+                        case 1:
+                            faeHP = 60;
+                            break;
+                        case 2:
+                            faeHP = 90;
+                            break;
+                        default:
+                            break;
+                    }
+                    switch(crand.Next(2))
+                    {
+                        case 0:
+                            faeDMG = 20;
+                            break;
+                        case 1:
+                            faeDMG = 40;
+                            break;
+                        case 2:
+                            faeDMG = 60;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+
+            }
+            else if (!TrainingRoom.Contents.ContainsKey(choice))           
+            {
+
             }
             player._RoomsCleared++;
+            Console.WriteLine("\n\n\n\n");
             goto NewRoom;
         }
     }
