4d95c4f3f91807380fc4b764be0bfc8b98865618
Alex Wilkinson <alex.wilk@outlook.com>
2020-10-31 13:39:37 +0000

    Commit 1
    Added Processor Class
    Added TextProcessor

---
+++ FaeriesAndTheWizard/FaeriesAndTheWizard/Processor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FaeriesAndTheWizard
+{
+    static class Processor
+    {
+        internal static void ProcessText(string s)
+        {
+            foreach (char c in s)
+            {
+                Console.Write(c);
+                Thread.Sleep(10);
+            }
+        }
+    }
+}
