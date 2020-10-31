f94fcf8131ac002b188cb3f7055b31475f37f8f6
CastroLedger <castroledger@gmail.com>
2020-10-31 19:18:13 +0000

    Commit 21

--- FaeriesAndTheWizard/FaeriesAndTheWizard/Faerie.cs
+++ FaeriesAndTheWizard/FaeriesAndTheWizard/Faerie.cs
@@ -20,5 +20,20 @@ namespace FaeriesAndTheWizard
 
             _Score = ((_Health + _Damage) / 2);
         }
+
+        Random rand = new Random();
+
+        internal int Swipe()
+        {
+            Processor.ProcessText($"The fae swipes at you for {(_Damage / 2)} points of damage", 10);
+            return (_Damage / 2);
+        }
+
+        internal int MagicBlast()
+        {
+            int finalDamage = ((_Damage * 3) / (rand.Next(2,4)));
+            Processor.ProcessText($"The fae blasts you with magic, dealing {finalDamage} points of Damage", 10);
+            return finalDamage;
+        }
     }
 }
